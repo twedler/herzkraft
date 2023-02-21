88f5eab91c4890d7ece81f8ab7819aa112e28e9d
Thomas Wedler <mail@thomas-wedler.de>
2023-02-21 21:03:41 +0000

    import HKImage in yin page

--- gatsby/src/pages/yin-yoga.js
+++ gatsby/src/pages/yin-yoga.js
@@ -5,6 +5,7 @@ import BlockContent from '@sanity/block-content-to-react';
 import styled from 'styled-components';
 import Section, { SectionContent } from '../components/Section';
 import Hero from '../components/Hero';
+import HKImage from '../components/HKImage';
 import SEO from '../components/SEO';
 import EventList from '../components/EventList';
 import PriceList from '../components/PriceList';
