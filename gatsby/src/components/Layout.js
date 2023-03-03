432f082971832594a18b8b961c51e4cc7de0b322
Thomas Wedler <mail@thomas-wedler.de>
2023-03-03 16:19:33 +0000

    add ga

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -10,6 +10,17 @@ import DesignTokens from '../styles/DesignTokens';
 export default function Layout({ children }) {
   return (
     <>
+      <script async src="https://www.google-analytics.com/analytics.js" />
+      <script>
+        {`
+          window.ga=window.ga||function()
+          {(ga.q = ga.q || []).push(arguments)}
+          ;ga.l=+new Date; ga('create',
+          '258987239', 'auto'); ga('send',
+          'pageview');
+        `}
+      </script>
+      
       {/* Styles */}
       <DesignTokens />
       <GlobalStyles />
