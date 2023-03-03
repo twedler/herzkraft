ab55865cf6cf3b8dd0de89d24ab949cb1bb8e51d
Thomas Wedler <mail@thomas-wedler.de>
2023-03-03 16:01:58 +0000

    added test forms

---
+++ gatsby/src/pages/formulare.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Section, SectionContent } from '../components/Section';
+
+export default function FormularePage() {
+  return (        
+      <Section>
+        <SectionContent>
+          <h1>Formulare</h1>
+          <h2>----------</h2>
+          <div class="_form_1"></div><script src="https://nadineseuffert.activehosted.com/f/embed.php?id=1" type="text/javascript" charset="utf-8"></script>
+          <h2>----------</h2>
+          <div class="_form_3"></div><script src="https://nadineseuffert.activehosted.com/f/embed.php?id=3" type="text/javascript" charset="utf-8"></script>
+        </SectionContent>
+      </Section>
+  );
+}
