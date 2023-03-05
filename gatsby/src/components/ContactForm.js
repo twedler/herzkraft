da3ad9858d4299100f39338691d048930d341190
Thomas Wedler <mail@thomas-wedler.de>
2023-03-05 20:33:29 +0000

    try useffect on contact form

--- gatsby/src/components/ContactForm.js
+++ gatsby/src/components/ContactForm.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Helmet } from 'react-helmet';
 import Section, { SectionContent } from './Section';
 
 export default function ContactForm() {
+
+  useEffect(() => {
+    const script = document.createElement('script');
+  
+    script.src = "https://nadineseuffert.activehosted.com/f/embed.php?id=3";
+    script.async = true;
+  
+    document.body.appendChild(script);
+  
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
+
   return (        
       <Section>
         <SectionContent>
           <div className="_form_3"></div>
-
-          <Helmet>
-            <script async src="https://nadineseuffert.activehosted.com/f/embed.php?id=3" type="text/javascript" charSet="utf-8" />
-          </Helmet>
         </SectionContent>
       </Section>
   );
