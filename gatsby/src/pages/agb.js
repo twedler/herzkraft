add06779f748e12e3178528f2ec466eb0fc61065
Thomas Wedler <mail@thomas-wedler.de>
2024-01-27 18:43:39 +0100

    Update agb.js

--- gatsby/src/pages/agb.js
+++ gatsby/src/pages/agb.js
@@ -11,106 +11,114 @@ export default function AGBPage() {
         <SectionContent>
           <h1>Allgemeine Geschäfts- und Teilnahmebedingungen</h1>
           <h2>1. Geltung</h2>
-          <p>
-            Mit Anmeldung bei Herzkraft Yoga – Nadine Seuffert treten die
-            folgenden allgemeinen Geschäftsbedingungen in Kraft.
-          </p>
-          <h2>2. Inhalt des Online Angebots</h2>
-          <p>
-            Herzkraft Yoga – Nadine Seuffert bietet kostenpflichtige Online Yoga
-            Kurse über die Domain{' '}
-            <a href="https://www.nadineseuffert.com">www.nadineseuffert.com</a>{' '}
-            an. Die Kurse finden per Live Übertragung statt. Die/der
-            Teilnehmer*in nimmt von zu Hause digital teil. Der Kurs kann keine
-            medizinische Behandlung ersetzen. Die/der Teilnehmer*in verpflichtet
-            sich, bei möglichen negativen Auswirkungen unverzüglich
-            medizinischen Rat einzuholen.
-          </p>
-          <h2>3. Anmeldung & Nutzungsrecht</h2>
-          <p>
-            Die Anmeldung erfolgt über die Website{' '}
-            <a href="https://www.nadineseuffert.com">www.nadineseuffert.com</a>{' '}
-            an <br />
-            <br />
-            Herzkraft Yoga – Nadine Seuffert <br />
-            Joseph-Haydn-Str. 9 <br />
-            68165 Mannheim <br />
-            <br /> Eine Anmeldung kommt durch Annahme der Allgemeinen
-            Geschäftsbedingungen, der darin erklärten Teilnahmebedingungen und
-            Datenschutzbestimmungen zustande. Die Anmeldung berechtigt zur
-            Teilnahme am Online Yoga Unterricht via Zoom. Nach erfolgreicher
-            Anmeldung wird ein Zugangslink mit Zugangsdaten per E-Mail
-            verschickt.
-          </p>
-          <h2>4. Teilnahmebedingungen</h2>
-          <p>
-            Die/der Teilnehmer*in verpflichtet sich, vor Kursbeginn über
-            bestehende Krankheiten, Beschwerden oder eine Schwangerschaft zu
-            informieren. Mit der Anmeldung bestätigt die/der Teilnehmer*in, dass
-            sie/er derzeit unter keinerlei akuten behandlungsbedürftigen
-            physischen oder psychischen Erkrankungen leidet. Die/der
-            Teilnehmer*in nutzt das Online Yoga Angebot mit größter Achtsamkeit
-            gegenüber dem eigenen Körper.
-          </p>
-          <h2>5. Pflichten der/des Teilnehmerin/ers</h2>
-          <p>
-            Die/der Teilnehmer*in ist verpflichtet, richtige und vollständige
-            Angaben zur eigenen Person zu machen. Die/der Teilnehmer*in ist
-            verpflichtet, die Zugangslinks und Kennwörter zu Online Kursen nur
-            für die eigene Teilnahme zu nutzen und nicht an Dritte
-            weiterzugeben.
-          </p>
-          <h2>6. Preise und Zahlungsbedingungen</h2>
-          <p>
-            Die Preise richten sich nach der unter{' '}
-            <a href="https://www.nadineseuffert.com">www.nadineseuffert.com</a>{' '}
-            einzusehenden aktuellen Preisliste. Die Preise gelten für 90 Minuten
-            Herzkraft Yoga bzw. 60 Minuten LateNightYoga. Der Betrag ist vor
-            Kursbeginn zu entrichten. Geht die Zahlung nicht ein, ist Herzkraft
-            Yoga – Nadine Seuffert nicht zur Leistungsbringung verpflichtet.
-          </p>
-          <h2>7. Termine</h2>
-          <p>
-            Die aktuellen Termine sind auf{' '}
-            <a href="https://www.nadineseuffert.com">www.nadineseuffert.com</a>{' '}
-            zu finden. Herzkraft Yoga behält sich vor, Termine in zumutbarer
-            Weise zu ändern. Die Zahlungsbedingungen bleiben davon unberührt.
-          </p>
-          <h2>8. Beendigung der Teilnahme</h2>
-          <p>
-            Herzkraft Yoga – Nadine Seuffert behält sich das Recht zur
-            außerordentlichen Kündigung aus wichtigem Grund vor. Ein wichtiger
-            Grund liegt insbesondere dann vor, wenn die/der Teilnehmer*in bei
-            Vertragsanbahnung falsche Angaben zu seiner Identität oder sonstigen
-            Daten macht. Die Teilnahme kann von Teilnehmer*innen jederzeit durch
-            eine schriftliche Mitteilung von der registrierten E-Mail-Adresse an
-            herzkraft@nadineseuffert.com beendet werden.
-          </p>
-          <h2>9. Haftung</h2>
-          <p>
-            Die Teilnahme an Herzkraft Yoga sowie LateNightYoga Online Kursen
-            erfolgt auf eigene Verantwortung. Eine Haftung von Herzkraft Yoga –
-            Nadine Seuffert für Folgen, die aus der Nutzung der Yoga Online
-            Seminare entstehen, besteht nicht. Während der gesamten Kursdauer
-            übernimmt der/die Teilnehmer*in die Verantwortung für sich selbst.
-            <br />
-            Herzkraft Yoga – Nadine Seuffert übernimmt keine Gewähr für
-            Erreichbarkeit, technische Verwertbarkeit und Virenfreiheit der
-            übermittelten Dateien.
-          </p>
-          <h2>10. Datenschutz</h2>
-          <p>
-            Die Nutzer erklären sich in diesem Umfang mit der Bearbeitung und
-            Nutzung ihrer personenbezogenen Daten einverstanden. Diese werden
-            ausschließlich zu Bearbeitungszwecken gespeichert. Die Bestimmungen
-            des Datenschutzgesetzes werden beachtet. Es werden keinerlei
-            personenbezogene Daten an Dritte weitergeleitet.
-          </p>
-          <h2>11. Nebenabreden</h2>
-          <p>
-            Mündliche Nebenabreden werden nicht getroffen. Änderungen und
-            Ergänzungen dieser AGB bedürfen der Schriftform.
-          </p>
+
+<p>Die AGB (Allgemeinen Gesch&auml;ftsbedingungen) gelten f&uuml;r alle Yoga und Coaching Einheiten von Herzkraft Yoga &ndash; Nadine Seuffert und ihren Klient:innen.</p>
+
+<h2>2. Inhalt des Online Angebots</h2>
+
+<p>Jede:r Klient:in erkl&auml;rt verbindlich mit ihrer/seiner Anmeldung, dass sie/er physisch gesund und psychisch stabil, sowie den einschl&auml;gigen k&ouml;rperlichen und geistigen Anforderungen gewachsen ist. Die angebotenen Leistungen verstehen sich nicht als Therapie- oder Heilprogramm. Sie ersetzen in keiner Weise eine &auml;rztliche Versorgung oder Verordnung von Medikamenten. Im Zweifelsfall ist eine &Auml;rztin oder ein Arzt zu konsultieren.</p>
+
+<p>Die Anmeldung zu Yoga Einheiten erfolgt &uuml;ber die Website <a href="http://www.nadineseuffert.com/" target="_blank">www.nadineseuffert.com</a>. Die Anmeldung zu Coaching Einheiten erfolgt per E-Mail-Absprachen. Calendly-Termin-Anfragen werden erst nach Best&auml;tigung durch Herzkraft Yoga &ndash; Nadine Seuffert g&uuml;ltig.</p>
+
+<p>Gleichzeitig werden mit der Anmeldung die AGB best&auml;tigt. Die/der Klient:in haftet f&uuml;r jegliche Kosten oder Folgen, die aufgrund von Unterlassung einer rechtzeitigen Bekanntgabe ge&auml;nderter Kontaktdaten entstehen k&ouml;nnen.</p>
+
+<p>Die Klient:innen tragen volle Verantwortung f&uuml;r die korrekte und vollst&auml;ndige Angabe der Kontaktdaten, sowie der Bekanntgabe jeglicher &Auml;nderungen.</p>
+
+<p>Die Anmeldung berechtigt zur Teilnahme am Online Yoga Unterricht via Zoom. Nach erfolgreicher Anmeldung wird ein Zugangslink mit Zugangsdaten per E-Mail verschickt.</p>
+
+<p>Die/der Klient:in ist verpflichtet, die Zugangslinks und Kennw&ouml;rter zu Online Kursen nur f&uuml;r die eigene Teilnahme zu nutzen und nicht an Dritte weiterzugeben.</p>
+
+<p>Die Kurse finden per Live &Uuml;bertragung statt. Die/der Klient:in nimmt von zu Hause digital teil. F&uuml;r eine Internetverbindung mit gen&uuml;gend Bandbreit ist der/die Klient:in selbst zust&auml;ndig.</p>
+
+<h2>3. Ausschluss</h2>
+
+<p>Die AGBs gelten als Bestandteil der Vereinbarung. Bei schwerwiegendem Versto&szlig; gegen diese Bedingungen ist ein Ausschluss vom Angebot der/des Klientin/Klienten gerechtfertigt. In diesem Fall ist der volle Preis zu bezahlen, bereits bezahlte Geb&uuml;hren, sowie anf&auml;llig gew&auml;hrte Ratenzahlungen, werden nicht r&uuml;ckerstattet.</p>
+
+<h2>4. Termine, Absagen oder Verschiebung</h2>
+
+<p>Die aktuellen <strong>Yoga Termine</strong> sind auf <a href="http://www.nadineseuffert.com/" target="_blank">www.nadineseuffert.com</a> zu finden. Herzkraft Yoga &ndash;Nadine Seuffert beh&auml;lt sich vor, Termine in zumutbarer Weise zu &auml;ndern. Die Zahlungsbedingungen bleiben davon unber&uuml;hrt.</p>
+
+<p>Herzkraft Yoga &ndash; Nadine Seuffert beh&auml;lt sich vor, Online Yoga-Einheiten bis 12 Uhr am Tag der Veranstaltung zu verschieben bzw. abzusagen.</p>
+
+<p>Teilnehmer:innen k&ouml;nnen bis 12 Uhr am Tag der Online Yoga Veranstaltung sich anmelden oder eine Anmeldung stornieren. Nach 12 Uhr am Tag der Online Veranstaltung ist bei vorheriger verbindlicher Anmeldung der volle Preis zu zahlen.</p>
+
+<p>Die Absprache der <strong>Coaching Termine</strong> finden auf individueller Basis per E-Mail statt. F&uuml;r die Coaching Termine gelten folgenden Regelungen:</p>
+
+<p>Bei einer Verschiebung durch den/die Klient:in bis zu 24 Stunden vorher kann der Termin kostenfrei verschoben werden. Danach wird der Termin storniert und die vollen Geb&uuml;hren werden berechnet.</p>
+
+<p>Bei einer Absage 72 Stunden vorher sind 50% der Geb&uuml;hren zu zahlen. Bei einer Absage 12 Stunden vorher wird der volle Preis berechnet.</p>
+
+<h2>5. Preis &amp; Zahlung</h2>
+
+<p>Jede Anmeldung ist rechtsverbindlich und beinhaltet daher die rechtm&auml;&szlig;ige Bezahlung der gesamten Geb&uuml;hr laut Ausschreibung.</p>
+
+<p>Die Geb&uuml;hr ist vorab und unverz&uuml;glich zu begleichen.</p>
+
+<p>Die/der Klient:in tr&auml;gt im Falle des Verzuges die Kosten der Eintreibung, der Mahnspesen und die Rechtsanwaltskosten. Die Teilnahme an dem jeweiligen Angebot ist erst wieder nach Begleichung ausst&auml;ndiger Zahlungen m&ouml;glich.</p>
+
+<h2>6. Gesundheitszustand</h2>
+
+<p>Die Klient:innen versichern, nicht an einer ansteckenden Krankheit zu leiden (nur bei Offline Angeboten) und dass dem Ausf&uuml;hren der Bewegungs&uuml;bungen keine medizinischen Gr&uuml;nde entgegenstehen.</p>
+
+<p>Chronische Erkrankungen, Erkrankungen des Bewegungsapparates sowie andere k&ouml;rperliche oder psychische Leiden, die die/den Klienten/in in ihrer/seiner</p>
+
+<p>Bewegungspraxis beeinflussen k&ouml;nnten, sind in der Kursleitung jeweils vor Stundenbeginn mitzuteilen. Auch das Vorliegen einer Schwangerschaft ist jeweils mit der Anmeldung unbedingt bekanntzugeben.</p>
+
+<p>Die/der Teilnehmer*in nutzt das Online Yoga Angebot mit gr&ouml;&szlig;ter Achtsamkeit gegen&uuml;ber dem eigenen K&ouml;rper.</p>
+
+<h2>7. &Auml;nderungen der AGB</h2>
+
+<p>&Auml;nderungen dieser Gesch&auml;ftsbedingungen werden der/dem Klienten/Klientin per E-Mail mitgeteilt. Widerspricht der/die Klienten/Klientin dieser &Auml;nderung nicht innerhalb von 4 Wochen nach Zugang der Mitteilung, gelten &Auml;nderungen als durch die/den Klienten/Klientin anerkannt. Auf das Widerspruchsrecht und die Rechtsfolgen des Schweigens werden die/der Klienten/Klientin im Falle der &Auml;nderung der Gesch&auml;ftsbedingungen noch gesondert hingewiesen.</p>
+
+<h2>8. Zustandekommen des Vertrags</h2>
+
+<p>Die auf der Webseite <a href="http://www.nadineseuffert.com/" target="_blank">www.nadineseuffert.com</a> aufgef&uuml;hrten Angebote stellen keine bindenden Angebote dar. Es handelt sich vielmehr um Aufforderungen an die/den Klientin/Klienten, ein verbindliches Angebot durch Absendung einer Bestellung zu unterbreiten.</p>
+
+<p>Die/der Klient:in kann ein verbindliches Angebot durch das Schreiben einer E-Mail bestellen. Den Zugang des Angebots wird Herzkraft Yoga &ndash; Nadine Seuffert per E-Mail best&auml;tigen.</p>
+
+<p>Herzkraft Yoga &ndash; Nadine Seuffert kann das Angebot der/des Kundin/Kunden durch eine schriftliche Auftragsbest&auml;tigung per E-Mail annehmen. Der Kaufvertrag kommt mit der Annahme der Bestellung zustande. Herzkraft Yoga &ndash; Nadine Seuffert ist ausdr&uuml;cklich auch berechtigt, die Anmeldung abzulehnen.</p>
+
+<p>Die Bestellabwicklung bzw. Anmeldung und etwaige Kontaktaufnahmen finden in der Regel per E-Mail statt. Die/der Klient:in sollte daher sicherstellen, dass die von ihm angegebene E-Mail-Adresse korrekt ist und Nachrichten empfangen werden k&ouml;nnen.</p>
+
+<p>Herzkraft Yoga &ndash; Nadine Seuffert beh&auml;lt sich das Recht zur au&szlig;erordentlichen K&uuml;ndigung aus wichtigem Grund vor. Ein wichtiger Grund liegt insbesondere dann vor, wenn die/der Teilnehmer*in bei Vertragsanbahnung falsche Angaben zu seiner Identit&auml;t oder sonstigen Daten macht. Die Teilnahme kann von Klient:innen jederzeit durch eine schriftliche Mitteilung von der registrierten E-Mail-Adresse an <a href="mailto:herzkraft@nadineseuffert.com" target="_blank">herzkraft@nadineseuffert.com</a> beendet werden.</p>
+
+<h2>9. Widerruf und Stornierung bei Coaching Vertr&auml;gen</h2>
+
+<p>Sie haben das Recht, binnen 14 Tagen ohne Angaben von Gr&uuml;nden diesen Vertrag zu widerrufen. Es wird f&uuml;r die Rechtswirksamkeit von Widerrufserkl&auml;rungen ausdr&uuml;cklich die Schriftform vereinbart. Um Ihr Widerrufsrecht auszu&uuml;ben, m&uuml;ssen Sie Herzkraft Yoga &ndash;Nadine Seuffert mittels einer eindeutigen Erkl&auml;rung per E-Mail &uuml;ber Ihren Entschluss, diesen Vertrag zu widerrufen, informieren. Zur Wahrung der Widerrufsfrist reicht es aus, dass Sie die Mitteilung &uuml;ber die Aus&uuml;bung des Widerrufsrechts vor Ablauf der Widerrufsfrist absenden.</p>
+
+<p>Bei Widerruf des Vertrags binnen 14 Tagen, werden Ihnen die bereits geleisteten Geb&uuml;hren, die wir von Ihnen erhalten haben, unverz&uuml;glich und sp&auml;testens binnen 14 Tagen ab dem Tag, an dem die Mitteilung &uuml;ber Ihren Widerruf dieses Vertrags bei uns eingegangen ist, r&uuml;ckerstattet. Die R&uuml;ckzahlung erfolgt auf dasselbe Zahlungsmittel, das sie bei der urspr&uuml;nglichen Transaktion eingesetzt haben, es sei denn, es wurde anderes vereinbart.</p>
+
+<h2>10. Copyright</h2>
+
+<p>S&auml;mtliche Unterlagen, in Form von Skripten, Videos, Audios, Seminaren etc. gelten als geistiges Eigentum von Herzkraft Yoga &ndash; Nadine Seuffert. Von ihr zur Verf&uuml;gung gestellte Unterlagen unterliegen dem Urheberrecht und d&uuml;rfen ausschlie&szlig;lich zum eigenen Gebrauch verwendet und nur mit vorheriger schriftlicher Zustimmung vervielf&auml;ltigt oder weitergegeben werden. Jeder Versto&szlig; gegen das Urheberrecht wird gerichtlich verfolgt.</p>
+
+<p>Foto-, Audio- und Videoaufzeichnung von Klient:innen und eine Weitergabe an Dritte oder anderweitige Nutzung bedarf einer schriftlichen Zustimmung.</p>
+
+<h2>11. Datenschutz</h2>
+
+<p>Die/der Klient:in erkl&auml;rt sich einverstanden, dass pers&ouml;nlichen Daten gespeichert und verwendet werden. Alle pers&ouml;nlichen Angaben der Klient:innen werden vertraulich behandelt und nicht an Dritte weitergegeben. Sie dienen ausschlie&szlig;lich internen Zwecken von Herzkraft Yoga &ndash; Nadine Seuffert zur gezielten Kundeninformation. Sind weitere Informationen nicht mehr erw&uuml;nscht, so kann die/der Klient:in dieses Einverst&auml;ndnis jederzeit widerrufen.</p>
+
+<p>Mit der Anmeldung wird ausdr&uuml;cklich die Ver&ouml;ffentlichung von Fotos, Film- und Tonaufnahmen, die im Rahmen des Angebots entstehen, freigegeben. Die/der Klient:in hat im Voraus das Recht einer derartigen Ver&ouml;ffentlichung zu widersprechen, wobei Herzkraft Yoga &ndash; Nadine Seuffert sich in diesem Fall vorbeh&auml;lt die/den Teilnehmer/in von dem Format auszuschlie&szlig;en. Ein Widerspruch ist nachtr&auml;glich nicht m&ouml;glich.</p>
+
+<p>Die/der Klient:in verpflichtet sich, &uuml;ber Aussagen und private Details, die im Rahmen der Angebote von anderen getroffen werden, Stillschweigen zu bewahren.</p>
+
+<h2>12. Haftung und Schadenersatz</h2>
+
+<p>Die Teilnahme an Angeboten von Herzkraft Yoga &ndash; Nadine Seuffert erfolgt auf eigene Gefahr. Schadenersatzanspr&uuml;che sind im gesetzlich zul&auml;ssigen Ausma&szlig; ausgeschlossen. Es wird keine Haftung f&uuml;r abhanden gekommene Gegenst&auml;nde, Gesundheitssch&auml;den sowie Unf&auml;lle jeglicher Art w&auml;hrend und nach der Veranstaltung gegen&uuml;ber Klient:innen und Dritten &uuml;bernommen. Eltern haften f&uuml;r ihre Kinder.</p>
+
+<p>Die von der Herzkraft Yoga &ndash; Nadine Seuffert durchgef&uuml;hrten Einheiten sind nicht als Ersatz f&uuml;r eine &auml;rztliche Beratung oder Behandlung durch ausgebildete und anerkannte Therapeutinnen/Therapeuten anzusehen. Die Teilnehmer/innen werden darauf hingewiesen, selbstst&auml;ndig etwa mit einer/einem &Auml;rztin/Arzt abzukl&auml;ren, ob sie physisch und psychisch bereit sind, an dem Angebot teilzunehmen.</p>
+
+<p>Haftungsanspr&uuml;che gegen&uuml;ber Herzkraft Yoga &ndash; Nadine Seuffert, welche sich auf Sch&auml;den materieller oder ideeller Art beziehen, die durch die Nutzung oder Nichtnutzung der dargebotenen Informationen verursacht wurden, sind grunds&auml;tzlich ausgeschlossen, sofern seitens Herzkraft Yoga &ndash; Nadine Seuffert kein nachweislich vors&auml;tzliches oder grob fahrl&auml;ssiges oder vors&auml;tzliches Verschulden vorliegt. Es wird keine Haftung f&uuml;r das Abhandenkommen pers&ouml;nlicher Gegenst&auml;nde oder f&uuml;r das Abhandenkommen bereitgestellte Unterlagen &uuml;bernommen.</p>
+
+<h2>13. Schriftform</h2>
+
+<p>M&uuml;ndliche Vereinbarungen werden zwischen den Vertragsteilen nicht getroffen. Vereinbart wird, dass Erg&auml;nzungen oder &Auml;nderungen dieser AGB der Schriftform bed&uuml;rfen. Dies gilt auch f&uuml;r das Abgehen des Schriftformerfordernisses selbst.</p>
+
+<h2>14. Salvatorische Klausel</h2>
+
+<p>Ist eine der vorstehenden Bestimmungen aufgrund gesetzlicher Bestimmungen, Vorschriften oder Gesetzes&auml;nderungen ganz oder teilweise unwirksam, bleiben alle anderen Bestimmungen hiervon unber&uuml;hrt und gelten weiterhin in vollem Umfang. In einem Fall derUnwirksamkeit einer einzelnen Bestimmung gilt die entsprechende gesetzliche Regelung.</p>
+
+<p><em>Stand 16.01.2024</em></p>
         </SectionContent>
       </TextSection>
     </>
