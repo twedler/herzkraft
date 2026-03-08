23b1b977efac6c7a0ebfc07863aa86e554731d98
Thomas Wedler <thomas.wedler@schott.com>
2026-03-08 13:50:43 +0100

    fix contact form styling

--- gatsby/src/components/ContactForm.js
+++ gatsby/src/components/ContactForm.js
@@ -1,26 +1,33 @@
 import React from "react";
+import styled from "styled-components";
 import Section, { SectionContent } from "./Section";
 
+const ContactSection = styled(Section)`
+  ${SectionContent} > p {
+    text-align: center;
+  }
+`;
+
 export default function ContactForm() {
-  const body = `Hallo liebe Nadine,%0d%0a%0d%0ameine Nachricht an dich: (bitte angeben)%0d%0a%0d%0aVielen Dank und viele Grüße%0d%0a%0d%0a`;
+  const body = `Hallo liebe Nadine,%0d%0a%0d%0ameine Nachricht an dich:%0d%0a%0d%0a(bitte angeben)%0d%0a%0d%0aVielen Dank und viele Grüße%0d%0a%0d%0a`;
 
   return (
-    <Section id="contact">
+    <ContactSection id="contact">
       <SectionContent>
         <h2>Kontakt</h2>
         <p>
           Von Herz zu Herz - falls Du Fragen zu meinen Angeboten oder sonst was
-          auf dem Herzen hast, schreib mir gerne eine Nachricht.
+          auf dem Herzen hast.
         </p>
         <p>
           <a
             className="button"
             href={`mailto:herzkraft@nadineseuffert.com?subject=Herzensbotschaft&body=${body}`}
           >
-            Absenden
+            Schreib mir gerne eine Nachricht
           </a>
         </p>
       </SectionContent>
-    </Section>
+    </ContactSection>
   );
 }
